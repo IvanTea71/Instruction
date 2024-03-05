fb9114cf4b2b73b7edae81234f5e69844684d18f
IvanTea71 <108534815+IvanTea71@users.noreply.github.com>
2024-03-05 19:51:03 +0300

    initialize

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] private Builder _builder;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _builder.StepChanged += ChangePosition;
+    }
+
+    private void OnDisable()
+    {
+        _builder.StepChanged -= ChangePosition;
+    }
+
+    private void ChangePosition(int number)
+    {
+        _animator.SetInteger("State", number);
+    }
+}
